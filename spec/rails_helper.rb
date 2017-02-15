5011cb3b3e254925d3e430bc6dccae23af26ab23
Shutkov Maksim <shutkov.maksim@mail.ru>
2017-02-15 17:42:32 +0300

    rails_apps_composer: set up database

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,20 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+
+    # Choose one or more libraries:
+    # with.library :active_record
+    # with.library :active_model
+    # with.library :action_controller
+    # Or, choose the following (which implies all of the above):
+    with.library :rails
+  end
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
