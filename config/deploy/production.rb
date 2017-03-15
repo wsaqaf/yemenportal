bf802201f542104bd44dbe4fcd321b9fd42fdb7e
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2017-03-15 16:22:02 +0300

    Puma should listen a port because requests will be proxied to our app via the firewall

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,7 @@ set :rails_env, 'production'
 set :puma_env, fetch(:rails_env)
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_bind, "tcp://0.0.0.0:8080"
 set :puma_threads, [2, 16]
 set :puma_workers, 2
 set :puma_preload_app, true
