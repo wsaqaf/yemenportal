cb3e483649ff99057830ba2dad6f803e8fe8fc80
Shutkov Maksim <shutkov.maksim@mail.ru>
2017-03-30 23:18:21 +0300

    Added categories to header of post

--- app/concepts/posts/post_item/cell.rb
+++ app/concepts/posts/post_item/cell.rb
@@ -1,7 +1,13 @@
 class Posts::PostItem::Cell < Application::Cell
   PREVIEW_SIZE = 180
 
-  property :title, :link, :published_at, :description, :id, :category_ids
+  property :title, :link, :published_at, :description, :id, :category_ids, :categories
+
+  private
+
+  def category_names
+    categories.map(&:name)
+  end
 
   def preview
     ActionView::Base.full_sanitizer.sanitize(description)[0..PREVIEW_SIZE].tr("\n", " ") + "..."
