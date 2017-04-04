e13d862d0c49445fa2a8417b751c1e97a911908f
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2017-04-04 11:33:40 +0300

    Enable turbolinks in order to get rid of page blinking

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,10 @@
+//= require turbolinks
 //= require jquery3
 //= require jquery_ujs
 //= require foundation
 //= require selectize
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+document.addEventListener("turbolinks:load", function() {
+  $(document).foundation();
+});
