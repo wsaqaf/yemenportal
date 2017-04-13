56ec15d3e40329589b597b34528d03c1875cf891
Shutkov Maksim <shutkov.maksim@mail.ru>
2017-04-13 13:45:15 +0300

    Added resent invite to moderators

--- spec/requests/moderators/invites_controller_request_spec.rb
+++ spec/requests/moderators/invites_controller_request_spec.rb
@@ -1,17 +1,16 @@
 require "rails_helper"
 
 describe Moderators::InvitesController, type: :request do
-  let(:user) { build(:user, role: "moderator", id: '1') }
+  let(:user) { build(:user, role: "moderator", id: "1") }
 
   describe "#create" do
     let(:do_request) { post "/moderators/#{user.id}/invites", headers: { "HTTP_REFERER" => "some_path" } }
 
     it "redirect to sources list" do
-      allow(User).to receive(:find).with('1').and_return(user)
+      allow(User).to receive(:find).with("1").and_return(user)
       expect(user).to receive(:invite!)
       sign_in user
       do_request
-
 
       expect(response).to redirect_to("some_path")
     end
