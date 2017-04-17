3f476c72b6670262c3dd37d408451ccd3fb00270
Shutkov Maksim <shutkov.maksim@mail.ru>
2017-04-17 13:55:22 +0300

    Update koala setting

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,13 +1,10 @@
 # In Rails, you could put this in config/initializers/koala.rb
+token = Koala::Facebook::OAuth.new(ENV["FACEBOOK_KEY"],  ENV["FACEBOOK_SECRET"]).get_app_access_token
+
 Koala.configure do |config|
-  config.access_token = '589532491165068|DmHvv4umqujsyatNKbX0LkJNG44'
-  config.app_access_token = '589532491165068|DmHvv4umqujsyatNKbX0LkJNG44'
-  config.app_id = '589532491165068'
-  config.app_secret = 'd1d811872c3e144d6e947858ac4a16fb'
+  config.access_token = token
+  config.app_access_token = token
 
-
-  # @oauth = Koala::Facebook::OAuth.new('589532491165068', 'd1d811872c3e144d6e947858ac4a16fb')
-  # @oauth.get_app_access_token
-  # a = Koala::Facebook::API.new('589532491165068|DmHvv4umqujsyatNKbX0LkJNG44','d1d811872c3e144d6e947858ac4a16fb')
-  # a.get_connection('ByRoR', 'posts')
+  config.app_id = ENV["FACEBOOK_KEY"]
+  config.app_secret = ENV["FACEBOOK_SECRET"]
 end
