7f076f14ce35e852f6fb09e816cc0e33762a5d97
Shutkov Maksim <shutkov.maksim@mail.ru>
2017-05-22 22:56:53 +0300

    Update config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server "inewscloud-vip.positive-dedicated.net", port: 33015, user: "yemenportal",
   roles: [:app, :web, :db]
 
-set :branch, "feature-add-post-clustering"
+set :branch, "master"
 
 set :rails_env, 'production'
