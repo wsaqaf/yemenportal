a0df6a6ba352c663e014681d7584a9eddfa83680
Shutkov Maksim <shutkov.maksim@mail.ru>
2017-07-05 13:50:24 +0300

    Updated fb script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,4 +9,26 @@
 
 document.addEventListener("turbolinks:load", function() {
   $(document).foundation();
+
+  var initializeFacebookSDK;
+
+  var params =  $('#fb-root').data()
+
+  initializeFacebookSDK = function() {
+    var ref, ref1;
+    FB.init({
+      appId: params.appId,
+      cookie: true,
+      xfmbl: true,
+      version: params.appVersion
+    });
+    return typeof FB !== "undefined" && FB !== null ? (ref1 = FB.XFBML) != null ? ref1.parse() : void 0 : void 0;
+  };
+
+  jQuery(function() {
+    delete FB;
+    return $.getScript("//connect.facebook.net/" + params.appLocale + "/sdk.js#xfbml=1&version=v2.3&appId=" + params.appId, function() {
+      return initializeFacebookSDK();
+    });
+  });
 });
