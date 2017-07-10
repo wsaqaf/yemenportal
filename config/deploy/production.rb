9394b574d49e28e7bf00f423f2bbfb8b4fb6b565
Shutkov Maksim <shutkov.maksim@mail.ru>
2017-07-10 21:54:42 +0300

    Fixed downed for prod

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server "inewscloud-vip.positive-dedicated.net", port: 33015, user: "yemenportal",
   roles: [:app, :web, :db]
 
-set :branch, "master"
+set :branch, "problem-crashed-prod"
 
 set :rails_env, 'production'
