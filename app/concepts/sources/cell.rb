cdb5b39955f4dbd0d3fa7351b2a0a513cf047bb8
Shutkov Maksim <shutkov.maksim@mail.ru>
2017-07-12 16:28:54 +0300

    Updated user permission for source list

--- app/concepts/sources/cell.rb
+++ app/concepts/sources/cell.rb
@@ -2,10 +2,10 @@ class Sources::Cell < Application::Cell
   private
 
   property :link
-  option :approve_state
+  option :approve_state, :current_user
 
   def table_body
-    concept("sources/item/cell", collection: model.to_a)
+    concept("sources/item/cell", collection: model.to_a, current_user: current_user)
   end
 
   def title
@@ -14,5 +14,21 @@ class Sources::Cell < Application::Cell
     else
       t("source.title.suggested")
     end
+  end
+
+  def suggest_button
+    button = link_to t("menu.sources.suggest_source"), new_sources_suggest_path, class: "button small float-left"
+    button = tooltip_wraper(button) unless policy(User).login?
+    button
+  end
+
+  def tooltip_wraper(button)
+    unless current_user
+      tolltip_title = t("user.should_login")
+      tooltip = "<span data-tooltip aria-haspopup='true' data-tooltip='' class='has-tip float-left top' \
+        title='#{tolltip_title}'>"
+      button = tooltip + button + "</span>"
+    end
+    button
   end
 end
