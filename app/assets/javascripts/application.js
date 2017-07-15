0014624b2117cb30b830c938f81b399aa5d4ca11
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2017-07-15 16:25:50 +0300

    Make facebook SDK to work with turbolinks
    Turbolinks doesn't reload page on each page visit. It just changes only
    body of document. So it doesn't reexecute sources listed in head.
    
    When facebook SDK is added to head, it changes #fb-root element in
    body. In order to make Facebook SDK work on multiple pages w/o
    reexecuting script in head we have to keep #fb-root consistent on all
    the pages.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,28 +7,18 @@
 //= require moment/ar-tn.js
 //= require_tree .
 
+window.facebookApi = new FacebookApi();
+
+document.addEventListener("turbolinks:before-visit", function() {
+  window.facebookApi.saveFbRootBeforeBodyChange(document);
+});
+
+document.addEventListener("turbolinks:before-render", function(event) {
+  window.facebookApi.restoreFbRootAfterBodyChange(event.data.newBody);
+});
+
 document.addEventListener("turbolinks:load", function() {
   $(document).foundation();
 
-  var initializeFacebookSDK;
-
-  var params =  $('#fb-root').data()
-
-  initializeFacebookSDK = function() {
-    var ref, ref1;
-    FB.init({
-      appId: params.appId,
-      cookie: true,
-      xfmbl: true,
-      version: params.appVersion
-    });
-    return typeof FB !== "undefined" && FB !== null ? (ref1 = FB.XFBML) != null ? ref1.parse() : void 0 : void 0;
-  };
-
-  jQuery(function() {
-    delete FB;
-    return $.getScript("//connect.facebook.net/" + params.appLocale + "/sdk.js#xfbml=1&version=v2.3&appId=" + params.appId, function() {
-      return initializeFacebookSDK();
-    });
-  });
+  window.facebookApi.parseFbML();
 });
