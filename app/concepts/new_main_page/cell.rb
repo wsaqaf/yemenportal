5a14ac0df397e1645dacd1794f38c0495e8fed04
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2017-08-07 16:03:14 +0300

    Show when current user voted post

--- app/concepts/new_main_page/cell.rb
+++ app/concepts/new_main_page/cell.rb
@@ -4,4 +4,12 @@ class NewMainPage::Cell < Application::Cell
   def topics
     model
   end
+
+  def upvoted_class_if_upvoted(topic)
+    "js-upvoted" if topic.upvoted_by_user?
+  end
+
+  def downvoted_class_if_downvoted(topic)
+    "js-downvoted" if topic.downvoted_by_user?
+  end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -17,16 +17,31 @@ class Topic < ApplicationRecord
   has_many :posts
   has_many :votes, dependent: :destroy
 
-  scope :ordered_by_date, -> { order("updated_at DESC") }
+  scope :ordered_by_date, -> { order("created_at DESC") }
+
+  def self.include_voted_by_user(user)
+    joins("LEFT JOIN votes ON votes.topic_id = topics.id AND votes.user_id = #{user.id}")
+      .group(:id).select("topics.*,
+        (COUNT(votes.*) > 0 AND SUM(votes.value) > 0) AS upvoted_by_user,
+        (COUNT(votes.*) > 0 AND SUM(votes.value) < 0) AS downvoted_by_user")
+  end
+
+  def upvoted_by_user?
+    attributes["upvoted_by_user"] || false
+  end
+
+  def downvoted_by_user?
+    attributes["downvoted_by_user"] || false
+  end
 
   def initial_post
-    posts.latest
+    posts.min_by { |post| post.created_at }
   end
 
   delegate :title, :source_name, :category_names, :description, :image_url, to: :initial_post
 
   def related_posts
-    []
+    posts - [initial_post]
   end
 
   def update_voting_result(new_value)
