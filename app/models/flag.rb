5cdebf348d9d1cc1f60f98cfd7c71d4bbef5f6f5
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2017-08-11 13:07:46 +0300

    Flag model is able to provide number of reviews for a topic and reviewed? boolean flag
    We will need this on topic reviews page in order to display flags
    differently.

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -10,4 +10,23 @@
 
 class Flag < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
+  def self.include_number_of_reviews_for_topic(topic)
+    joins("LEFT JOIN reviews ON reviews.flag_id = flags.id AND reviews.topic_id = #{topic.id}")
+      .group(:id).select("flags.*, COUNT(reviews.*) AS number_of_reviews_for_topic")
+  end
+
+  def self.include_topic_reviewed_by_user(topic, user)
+    joins("LEFT JOIN reviews ON reviews.flag_id = flags.id
+      AND reviews.topic_id = #{topic.id} AND reviews.moderator_id = #{user.id}")
+      .group(:id).select("flags.*, (COUNT(reviews.*) > 0) AS topic_reviewed_by_user")
+  end
+
+  def number_of_reviews_for_topic
+    attributes["number_of_reviews_for_topic"] || 0
+  end
+
+  def topic_reviewed_by_user?
+    attributes["topic_reviewed_by_user"] || false
+  end
 end
