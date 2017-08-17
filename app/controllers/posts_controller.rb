536454303acc6d351cb976c895ed99cf6b78b483
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2017-08-17 21:20:48 +0300

    Temporary disable showing posts in iframe
    Because we didn't install proxy on server due to security reasons. We'll
    find another way to render it in iframe.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,17 @@
 class PostsController < ApplicationController
   def show
-    render cell: :show, model: post, layout: "iframe_layout"
+    if render_in_iframe?
+      render cell: :show, model: post, layout: "iframe_layout"
+    else
+      redirect_to post.link
+    end
   end
 
   private
+
+  def render_in_iframe?
+    FeatureToggle.iframe_proxy_enabled? || post.link.start_with?("https")
+  end
 
   def post
     Post.find(params[:id])
