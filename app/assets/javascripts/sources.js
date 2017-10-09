944b4ee0cc0fbd8d0d11d2f9d14c6d16172e0fd5
Andrey Tatarenko <atatarenko@github.com>
2017-10-09 15:44:56 +0300

    Add input unfocus on enter press in order to update url by enter

--- app/assets/javascripts/sources.js
+++ app/assets/javascripts/sources.js
@@ -1,4 +1,6 @@
 document.addEventListener("turbolinks:load", function() {
+  const ENTER_KEY_CODE = 13;
+
   $('.sources .textarea').each(function() {
     var $this = $(this);
     $this[0].defaultValue = $this.text();
@@ -7,46 +9,49 @@ document.addEventListener("turbolinks:load", function() {
   $('.sources input').focusout(function(){
     var $this = $(this);
     var new_value = $this.val();
-    var name = $this[0].name
+    var name = $this[0].name;
 
     doRequest($this, new_value, name)
-  })
+  });
 
   $('.sources .textarea').focusout(function(){
     var $this = $(this);
-    var new_value = $this.text()
-    var name = $this[0].getAttribute('name')
+    var new_value = $this.text();
+    var name = $this[0].getAttribute('name');
 
     doRequest($this, new_value, name)
-  })
+  });
+
+  $('.sources input').keypress(function (event) {
+    if (event.keyCode === ENTER_KEY_CODE) {
+        $(this).blur();
+    }
+  }).bind(ENTER_KEY_CODE);
 
 
-  doRequest = function($this, new_value, name) {
-    var post_id = $this.closest('.sources').find('input:hidden')[0].id;
-    var post_data = {}
-    post_data[name] = new_value
+  doRequest = function($this, newValue, name) {
+    var postId = $this.closest('.sources').find('input:hidden')[0].id;
+    var postData = {};
+    postData[name] = newValue;
 
     $.ajax({
-      url: ("/api/sources/" + post_id),
+      url: ("/api/sources/" + postId),
       dataType: "script",
       method: "PUT",
-      data: post_data
+      data: postData
     }).fail(function(xhr) {
-      var errors = $.parseJSON(xhr.responseText).errors
-      if ($this.next('small').length != 0) {
+      var errors = $.parseJSON(xhr.responseText).errors;
+      if ($this.next('small').length !== 0) {
         $this.next('small').remove();
       }
-
-      if (errors[name] != undefined) {
+      if (errors[name] !== undefined) {
         $this.after("<small class='error'>" + errors[name] + "</small>");
       }
       $this.val($this.prop("defaultValue"));
     }).done(function() {
-      if ($this.next('small').length != 0) {
+      if ($this.next('small').length !== 0) {
         $this.next('small').remove();
       }
     });
-
-    true;
   }
 });
