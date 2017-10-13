ee9a833cf85196ba1ced501d4829f614a027318e
Andrey Tatarenko <atatarenko@github.com>
2017-10-13 18:49:23 +0300

    Add redirect to secured proxy connection

--- app/controllers/proxy/posts_controller.rb
+++ app/controllers/proxy/posts_controller.rb
@@ -1,6 +1,10 @@
 class Proxy::PostsController < ApplicationController
   def show
-    render html: post.page_content
+    if post.link.start_with?("https")
+      redirect_to post.link
+    else
+      render html: post.page_content
+    end
   end
 
   private
