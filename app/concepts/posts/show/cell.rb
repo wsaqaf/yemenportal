0cc0b9572c7dc7bf103709ceb5942d1552ae5a2a
Andrey Tatarenko <atatarenko@github.com>
2017-10-16 19:23:35 +0300

    Move embedded page rendering configuration to a single file

--- app/concepts/posts/show/cell.rb
+++ app/concepts/posts/show/cell.rb
@@ -6,11 +6,11 @@ class Posts::Show::Cell < Application::Cell
   end
 
   def header_url
-    post_header_url(post, protocol: protocol)
+    post_header_url(post, protocol: post_header_protocol)
   end
 
-  def protocol
-    ::Rails.env.production? ? "https" : "http"
+  def post_header_protocol
+    ::Rails.application.config_for(:external_site_embedding)["post_header_protocol"]
   end
 
   delegate :link, to: :post, prefix: true, allow_nil: true
