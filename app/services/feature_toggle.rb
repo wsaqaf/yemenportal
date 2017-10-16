0f9d3637eff7ab9709699daa317e61de96349a7b
Andrey Tatarenko <atatarenko@github.com>
2017-10-16 10:04:32 +0300

    Remove redundant external page proxying

--- app/services/feature_toggle.rb
+++ app/services/feature_toggle.rb
@@ -2,8 +2,4 @@ class FeatureToggle
   def self.clustering_enabled?
     ENV["FT_CLUSTERING_ENABLED"] == "true"
   end
-
-  def self.iframe_proxy_enabled?
-    ENV["FT_IFRAME_PROXY_ENABLED"] == "true"
-  end
 end
