6ccf9ad417f21553b53b4f569c6e60a1a0b40568
Andrey Tatarenko <atatarenko@github.com>
2017-10-19 16:19:11 +0300

    Fix long execution time of topic main post migration

--- db/migrate/20171018105726_add_main_post_to_topic.rb
+++ db/migrate/20171018105726_add_main_post_to_topic.rb
@@ -1,15 +1,10 @@
 class AddMainPostToTopic < ActiveRecord::Migration[5.0]
-  class Topic < ApplicationRecord
-    has_many :posts, dependent: :destroy
-
-    def initial_post
-      posts.min_by(&:created_at)
-    end
-  end
+  class Topic < ApplicationRecord; end
 
   def up
     add_reference :topics, :main_post, foreign_key: { to_table: :posts }, on_delete: :cascade
-    Topic.find_each { |topic| TopicMainPostMigration.new(topic).up }
+    set_main_post_if_initial_post_exists
+    remove_topics_without_main_post
     change_column_null :topics, :main_post_id, true
   end
 
@@ -17,25 +12,20 @@ class AddMainPostToTopic < ActiveRecord::Migration[5.0]
     remove_reference :topics, :main_post
   end
 
-  class TopicMainPostMigration
-    def initialize(topic)
-      @topic = topic
-    end
+  private
 
-    def up
-      if initial_post_id.present?
-        topic.update(main_post_id: initial_post_id)
-      else
-        topic.destroy
-      end
-    end
+  def set_main_post_if_initial_post_exists
+    execute <<-SQL
+      UPDATE "topics" SET "main_post_id" = (
+        SELECT "posts"."id"
+        FROM "posts"
+        WHERE "posts"."topic_id" ="topics"."id"
+        ORDER BY "posts"."created_at", "posts"."id" ASC LIMIT(1)
+      )
+    SQL
+  end
 
-    private
-
-    attr_reader :topic
-
-    def initial_post_id
-      @_initial_post_id ||= topic.initial_post&.id
-    end
+  def remove_topics_without_main_post
+    Topic.where(main_post_id: nil).delete_all
   end
 end
