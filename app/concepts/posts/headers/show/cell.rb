5161d45d79bed2c0f8687ecbcb63af4e50c25a77
Andrey Tatarenko <atatarenko@github.com>
2017-10-27 15:29:11 +0300

    Change back button with logo image

--- app/concepts/posts/headers/show/cell.rb
+++ app/concepts/posts/headers/show/cell.rb
@@ -5,5 +5,9 @@ class Posts::Headers::Show::Cell < Application::Cell
     model
   end
 
+  def logo_srcset
+    "#{path_to_image('header_logo_2x.png')} 2x"
+  end
+
   delegate :link, to: :post, prefix: true, allow_nil: true
 end
