cc156b1b22c7b968bc27d856a8dd285a86d0bd9c
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2017-11-09 00:27:39 +0300

    HOTFIX: Cluster only most recent posts

--- app/services/posts_clusterizer.rb
+++ app/services/posts_clusterizer.rb
@@ -1,6 +1,6 @@
 class PostsClusterizer
   def self.clusterize!
-    Post.non_clustered_posts.find_each do |post|
+    Post.non_clustered_posts.where("posts.created_at > ?", 3.hours.ago).find_each do |post|
       PostsClusterizer::TopicFinder.new(post).attach_topic!
     end
   end
